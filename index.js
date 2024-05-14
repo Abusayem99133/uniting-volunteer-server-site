ed460413eaa9eb091370f6273b90deeb9f009fe7
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-14 16:22:15 +0600

    jwt signin done and token generated

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const port = process.env.PORT || 5000;
 
 // middleware
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -41,6 +46,13 @@ async function run() {
       const user = req.body;
       console.log('user for token', user);
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
+      })
+      .send({success: true})
     })
 
     app.get('/volunteerNeeded', async(req, res)=>{
